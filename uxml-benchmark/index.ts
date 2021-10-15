b77cb4b17609c80ca23a0f1724828205f54868c9
FastAlien <3268092+FastAlien@users.noreply.github.com>
2021-10-15 17:48:06 +0200

    Add option to benchmark other libraries

--- uxml-benchmark/index.ts
+++ uxml-benchmark/index.ts
@@ -5,32 +5,49 @@ import { parse as fastXmlParse } from "fast-xml-parser";
 import { readFileSync } from "fs";
 import txml from "txml";
 
-if (process.argv.length !== 3) {
+if (process.argv.length < 3 && process.argv.length > 4) {
   console.error("Incorrect number of parameters");
   process.exit(1);
 }
 
-const suite = new Suite("XML parser benchmark", {
-  onStart: () => console.log("Running Suite"),
-  onError: () => console.log("Error in Suite"),
-  onAbort: () => console.log("Aborting Suite"),
-  onComplete: () => {
-    suite.forEach((target: Target) => {
-      console.log(`${target.name} : ${target.hz} requests/second`);
-    });
-  }
-});
+const [, , xmlFile, option] = process.argv;
+let benchmarkOtherLibs = false;
 
-const xmlFile = process.argv[2];
+if (option) {
+  switch (option) {
+    case "--all":
+      benchmarkOtherLibs = true;
+      break;
+    default:
+      console.error("Unknown option: ", option);
+      process.exit(1);
+  }
+}
+
 try {
   const xmlData = readFileSync(xmlFile, "utf-8");
-  const xml2jsParser = new Xml2JsParser();
+  const suite = new Suite("XML parser benchmark", {
+    onStart: () => console.log("Running Suite"),
+    onError: () => console.log("Error in Suite"),
+    onAbort: () => console.log("Aborting Suite"),
+    onComplete: () => {
+      suite.forEach((target: Target) => {
+        console.log(`${target.name} : ${target.hz} requests/second`);
+      });
+    }
+  });
 
-  suite.add("txml", () => txml.parse(xmlData))
-    .add("fast-xml-parser", () => fastXmlParse(xmlData))
-    .add("xml2js", () => xml2jsParser.parseString(xmlData))
-    .add("uxml", () => new XmlDocumentParser().parse(xmlData))
-    .run();
+  const uxmlParser = new XmlDocumentParser();
+  suite.add("uxml", () => uxmlParser.parse(xmlData));
+
+  if (benchmarkOtherLibs) {
+    suite.add("txml", () => txml.parse(xmlData));
+    suite.add("fast-xml-parser", () => fastXmlParse(xmlData));
+    const xml2jsParser = new Xml2JsParser();
+    suite.add("xml2js", () => xml2jsParser.parseString(xmlData));
+  }
+
+  suite.run();
 } catch (error) {
   console.error("Error running benchmark: ", error);
   process.exit(2);
