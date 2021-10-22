6b4b40b7949f3e93927e4a2b2851eb23065ea410
FastAlien <3268092+FastAlien@users.noreply.github.com>
2021-10-22 22:42:19 +0200

    Add unit tests for StringParser.moveTo()

--- uxml/parser/StringParser.test.ts
+++ uxml/parser/StringParser.test.ts
@@ -1,5 +1,41 @@
 import { NOT_FOUND, StringParser } from "uxml/parser/StringParser";
 
+describe("moveTo", () => {
+  it("should change position if value is >= 0 and <= string length", () => {
+    const parser = new StringParser("test");
+    parser.moveTo(0);
+    expect(parser.position).toBe(0);
+    parser.moveTo(4);
+    expect(parser.position).toBe(4);
+    parser.moveTo(2);
+    expect(parser.position).toBe(2);
+    parser.moveTo(3);
+    expect(parser.position).toBe(3);
+    parser.moveTo(0);
+    expect(parser.position).toBe(0);
+  });
+
+  it("should change position to 0 if value is < 0", () => {
+    const parser = new StringParser("test");
+    parser.moveTo(-1);
+    expect(parser.position).toBe(0);
+    parser.moveTo(-100);
+    expect(parser.position).toBe(0);
+    parser.moveTo(-Number.MAX_SAFE_INTEGER);
+    expect(parser.position).toBe(0);
+  });
+
+  it("should change position to string length if value is > string length", () => {
+    const parser = new StringParser("test");
+    parser.moveTo(5);
+    expect(parser.position).toBe(4);
+    parser.moveTo(100);
+    expect(parser.position).toBe(4);
+    parser.moveTo(Number.MAX_SAFE_INTEGER);
+    expect(parser.position).toBe(4);
+  });
+});
+
 describe("findFirst", () => {
   it("should return NOT_FOUND (-1) if character was not found", () => {
     expect(new StringParser("b").findFirst("z")).toBe(NOT_FOUND);
