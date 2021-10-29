f511cc27e240909535283a493f327aa3964f6cde
FastAlien <3268092+FastAlien@users.noreply.github.com>
2021-10-29 21:59:25 +0200

    Refactor unit tests for XmlDeclarationParser

--- uxml/parser/XmlDeclarationParser.test.ts
+++ uxml/parser/XmlDeclarationParser.test.ts
@@ -4,30 +4,32 @@ import { XmlDeclarationParser } from "uxml/parser/XmlDeclarationParser";
 
 it("should parse XML with version only", () => {
   const parser = new XmlDeclarationParser();
-  const expected: XmlDeclaration = {
-    version: "1.0"
-  };
   expect(parser.parse(new StringParser("<?xml version=\"1.0\"?>")))
-    .toEqual(expected);
+    .toEqual<XmlDeclaration>({
+      version: "1.0"
+    });
+
+  expect(parser.parse(new StringParser("<?xml version=\"1.1\"?>")))
+    .toEqual<XmlDeclaration>({
+      version: "1.1"
+    });
 });
 
 it("should parse XML with version and encoding", () => {
   const parser = new XmlDeclarationParser();
-  const expected: XmlDeclaration = {
-    version: "1.0",
-    encoding: "utf-8"
-  };
-  expect(parser.parse(new StringParser("<?xml version=\"1.0\" encoding=\"utf-8\"?>")))
-    .toEqual(expected);
+  expect(parser.parse(new StringParser("<?xml version=\"1.1\" encoding=\"utf-8\"?>")))
+    .toEqual<XmlDeclaration>({
+      version: "1.1",
+      encoding: "utf-8"
+    });
 });
 
 it("should parse XML with version, encoding and standalone", () => {
   const parser = new XmlDeclarationParser();
-  const expected: XmlDeclaration = {
-    version: "1.0",
-    encoding: "utf-8",
-    standalone: "yes"
-  };
-  expect(parser.parse(new StringParser("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>")))
-    .toEqual(expected);
+  expect(parser.parse(new StringParser("<?xml version=\"1.1\" encoding=\"utf-8\" standalone=\"yes\"?>")))
+    .toEqual<XmlDeclaration>({
+      version: "1.1",
+      encoding: "utf-8",
+      standalone: "yes"
+    });
 });
